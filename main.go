91d2ca0c667cb2924d29ba3e0c076a0118a63160
wasiq345 <wasiqazeem000@gmail.com>
2026-07-19 23:50:59 +0500

    added healthz endpoint

--- main.go
+++ main.go
@@ -7,12 +7,23 @@ import (
 
 func main() {
 	const port = "8080"
+	const filePath = "."
 	mux := http.NewServeMux()
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
+	fileserver := http.FileServer(http.Dir(filePath))
+	mux.Handle("/app/", http.StripPrefix("/app", fileserver))
+	mux.HandleFunc("/healthz", health)
+
 	println("Server Listening on port 8080")
 	log.Fatal(server.ListenAndServe())
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
