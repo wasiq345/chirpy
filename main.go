fd05b0b6309ddc213eff7f31aee44249873d13f3
wasiq345 <wasiqazeem000@gmail.com>
2026-07-22 20:40:49 +0500

    added createUsers endpoint and deleteUsers

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ import (
 	"os"
 	"strings"
 	"sync/atomic"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
@@ -18,10 +20,18 @@ import (
 type apiconfig struct {
 	FileServerHits atomic.Int32
 	DB             *database.Queries
+	Platform       string
 }
 
 type validation struct {
 	Body string `json:"body"`
+}
+
+type user struct {
+	Id         uuid.UUID `json:"id"`
+	Created_at time.Time `json:"created_at"`
+	Updated_at time.Time `json:"updated_at"`
+	Email      string    `json:"email"`
 }
 
 func (cfg *apiconfig) MiddleWareMetricInc(next http.Handler) http.Handler {
@@ -36,7 +46,7 @@ func main() {
 	dbURL := os.Getenv("DB_URL")
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
-		return
+		log.Fatal(err)
 	}
 	dbQueries := database.New(db)
 	const port = "8080"
@@ -51,8 +61,10 @@ func main() {
 	apiCfg := apiconfig{
 		FileServerHits: atomic.Int32{},
 		DB:             dbQueries,
+		Platform:       os.Getenv("PLATFORM"),
 	}
 	mux.Handle("/app/", apiCfg.MiddleWareMetricInc(http.StripPrefix("/app", fileserver))) //strip the url bcs fileServer search file in . directory not in /app/index.html
+	mux.HandleFunc("POST /api/users", apiCfg.createUsers)
 	mux.HandleFunc("GET /api/healthz", health)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.countRequests)
 	mux.HandleFunc("POST /admin/reset", apiCfg.ResetRequests)
@@ -62,11 +74,41 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
+func (apicfg *apiconfig) createUsers(w http.ResponseWriter, r *http.Request) {
+
+	User := user{}
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&User)
+
+	defer r.Body.Close()
+
+	if err != nil {
+		RespondWithErr(w, http.StatusBadRequest, "Couldn't Create User")
+		return
+	}
+
+	U, err := apicfg.DB.CreateUser(r.Context(), User.Email)
+	if err != nil {
+		RespondWithErr(w, http.StatusInternalServerError, "Couldn't Create User")
+		return
+	}
+
+	User.Created_at = U.CreatedAt
+	User.Updated_at = U.UpdatedAt
+	User.Id = U.ID
+	User.Email = U.Email
+
+	RespondWithJson(w, http.StatusCreated, User)
+}
+
 func ValidateChirp(w http.ResponseWriter, r *http.Request) {
 
 	valid := validation{}
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&valid)
+
+	defer r.Body.Close()
 
 	if err != nil {
 		RespondWithErr(w, http.StatusBadRequest, "Something went wrong")
@@ -132,8 +174,17 @@ func (cfg *apiconfig) countRequests(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiconfig) ResetRequests(w http.ResponseWriter, r *http.Request) {
+	if cfg.Platform != "dev" {
+		RespondWithErr(w, http.StatusForbidden, "you dont have the permissoin to access")
+		return
+	}
 	w.Header().Add("Content-Type", "text/plain; charset= utf-8")
 	w.WriteHeader(http.StatusOK)
 	cfg.FileServerHits.Swap(0)
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.FileServerHits.Load())))
+	err := cfg.DB.DeleteAllUser(r.Context())
+	if err != nil {
+		RespondWithJson(w, http.StatusInternalServerError, "unable to delete all users")
+		return
+	}
+	w.Write([]byte(fmt.Sprintf("Deleted ALl Users\nHits: %d", cfg.FileServerHits.Load())))
 }
