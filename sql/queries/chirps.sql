cd2a37a526ae9ebc5b5ae0f86b8a6c3ac9633cf5
wasiq345 <wasiqazeem000@gmail.com>
2026-07-22 22:20:21 +0500

    added truncate query

--- sql/queries/chirps.sql
+++ sql/queries/chirps.sql
@@ -3,3 +3,6 @@ INSERT INTO chirp (id, created_at, updated_at, body, userId) values (
     gen_random_uuid(), NOW(), NOW(), $1, $2
 )
 RETURNING *;
+
+-- name: DeleteAllChirps :exec
+TRUNCATE TABLE chirp;
