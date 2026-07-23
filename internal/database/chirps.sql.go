d8fb95af50c1e2888e444b8197780970aa3fd5ed
wasiq345 <wasiqazeem000@gmail.com>
2026-07-23 20:10:08 +0500

    added getAllchirps, getOnechirp go sql code

--- internal/database/chirps.sql.go
+++ internal/database/chirps.sql.go
@@ -44,3 +44,53 @@ func (q *Queries) DeleteAllChirps(ctx context.Context) error {
 	_, err := q.db.ExecContext(ctx, deleteAllChirps)
 	return err
 }
+
+const getAllChirps = `-- name: GetAllChirps :many
+SELECT id, created_at, updated_at, body, userid FROM chirp ORDER BY created_at
+`
+
+func (q *Queries) GetAllChirps(ctx context.Context) ([]Chirp, error) {
+	rows, err := q.db.QueryContext(ctx, getAllChirps)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []Chirp
+	for rows.Next() {
+		var i Chirp
+		if err := rows.Scan(
+			&i.ID,
+			&i.CreatedAt,
+			&i.UpdatedAt,
+			&i.Body,
+			&i.Userid,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const getOneChirp = `-- name: GetOneChirp :one
+SELECT id, created_at, updated_at, body, userid FROM chirp WHERE chirp.id = $1
+`
+
+func (q *Queries) GetOneChirp(ctx context.Context, id uuid.UUID) (Chirp, error) {
+	row := q.db.QueryRowContext(ctx, getOneChirp, id)
+	var i Chirp
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Body,
+		&i.Userid,
+	)
+	return i, err
+}
